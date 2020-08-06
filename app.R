3190ba13df598148c0defa9e2ecc2581a863a9f4
BrianLang <brianlang89@gmail.com>
2020-08-06 10:48:16 +0200

    prototype finished. added a couple of sample files to the folder so it's easy enough to flick through.

--- app.R
+++ app.R
@@ -1,95 +1,106 @@
 library(shiny)
 library(fit)
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(leaflet)
 library(sp)
+library(mapview)
 
-# Define UI for data upload app ----
 ui <- fluidPage(
 
-    # App title ----
-    titlePanel("Uploading Files"),
+    titlePanel("Visualize FIT file"),
 
-    # Sidebar layout with input and output definitions ----
+
     sidebarLayout(
 
-        # Sidebar panel for inputs ----
         sidebarPanel(
 
-            # Input: Select a file ----
             fileInput("file1", "Choose .fit File",
                       multiple = TRUE,
                       accept = c(".fit"))
 
         ),
 
-        # Main panel for displaying outputs ----
         mainPanel(
-            tableOutput("summary"),
-            # Output: Data file ----
-            tableOutput("record"),
+            fluidRow(tableOutput("summary"), align="center"), ## header
 
-            leafletOutput("map")
+            leafletOutput("map"), ## map with line
+
+            plotOutput("figures"), ## speed vs distance
+            hr(),
+            tags$footer("Brian M. Lang 2020", align = "center")
         )
+
     )
 )
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
 
+
+    ## user input file will be NULL to begin with, so use the example to start with, this will be shipped with the shiny app when uploaded to rstudio.io
     fit_file <- reactive({
-        fit::read.fit(input$file1$datapath)
+        if (is.null(input$file1)) {
+            fit::read.fit("./data/example.fit")
+        } else {
+            fit::read.fit(input$file1$datapath)
+        }
     })
 
-
     output$summary <- renderTable({
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
-        req(input$file1)
 
         return(fit_file() %>%
                    .$session %>%
-                   transmute(num_laps, start_time, total_calories,
-                             kilometers = total_distance/1000,
-                             minutes = total_elapsed_time/60))
+                   transmute(kilometers = total_distance/1000,
+                             minutes = total_elapsed_time/60,
+                             average_pace = (minutes)/kilometers,
+                             total_calories))
 
     })
     #
-    output$record <- renderTable({
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
-        req(input$file1)
-
-        return(fit_file() %>% .$record %>% head())
-
-    })
+    # output$record <- renderTable({
+    #     # input$file1 will be NULL initially. After the user selects
+    #     # and uploads a file, head of that data file by default,
+    #     # or all rows if selected, will be shown.
+    #     req(input$file1)
+    #
+    #     return(fit_file() %>% .$record %>% head())
+    #
+    # })
 
     output$map <- renderLeaflet({
-        req(input$file1)
-
         fitdata <- fit_file()
 
 
         mp <- sp::SpatialPoints(coords = fitdata$record %>%
-                              transmute(lng = position_long,
-                                     lat = position_lat) %>% filter(!is.na(lat)))
+                                    transmute(lng = position_long,
+                                              lat = position_lat) %>%
+                                    filter(!is.na(lat)))
         mp1 <- mapview::coords2Lines(coords = sp::coordinates(mp), ID = "A")
 
 
         return(leaflet::leaflet(mp1) %>%
-            leaflet::addTiles() %>%
-            leaflet::addPolylines(weight = 2))
+                   leaflet::addTiles() %>%
+                   leaflet::addPolylines(weight = 4, color = "black"))
+    })
 
-        #
-        #     geom_path(aes(x = lon, y = lat),
-        #               data = fitdata$record %>%
-        #                   mutate(lon = position_long,
-        #                          lat = position_lat) %>%
-        #                   filter(!is.na(lon)), size = 1, colour = 'blue')
+    output$figures <- renderPlot({
+        fitdata <- fit_file()
+
+        pdata <- fitdata$record %>%
+            filter(!is.na(position_long)) %>%
+            mutate(mpkm = 1000 / (speed * 60) )
+
+        pace_dist <- pdata %>%
+            ggplot(aes(distance/1000, mpkm)) +
+            geom_line() +
+            lims(y = c(0, 12)) +
+            theme_minimal() +
+            labs(y = "min/km", x = "distance (km)")
+
+        pace_dist
     })
 
 }
-# Run the app ----
+
 shinyApp(ui, server)
